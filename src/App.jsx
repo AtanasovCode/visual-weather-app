5800f833d8c0c00be6e031c5b14148a2df875126
AtanasovCode <a_ljupce@yahoo.com>
2024-10-14 13:47:44 +0200

    installed phosphor-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-
+import { FileCss } from "@phosphor-icons/react"
 
 const App = () => {
   return (
     <div className="bg-background text-text min-h-[100dvh] flex items-center justify-center">
+      <div className="flex items-center jsutify-center mr-6">
+        <FileCss 
+          size={64}
+          weight="fill"
+          color="#FFF"
+        />
+      </div>
       <div className="font-bold text-7xl text-center">
         Installed TailwindCSS
       </div>
