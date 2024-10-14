fa8a03110f7382fa3cd6cc66ad15fc27cbc57918
AtanasovCode <a_ljupce@yahoo.com>
2024-10-14 18:20:59 +0200

    created function to fetch and display data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,93 @@
 import { FileCss, Cat } from "@phosphor-icons/react";
+import { useEffect } from "react";
 import { useWeatherStore } from "./useWeatherStore";
 
 const App = () => {
 
   const {
-    cats,
+    loading,
+    setLoading,
+    weatherData,
+    setWeatherData,
+    location,
+    superSecretKey,
+
+    //weather values
+    temp,
+    setTemp,
+    conditions,
+    setConditions,
+    icon, setIcon,
   } = useWeatherStore();
 
+  const fetchWeatherData = async () => {
+    try {
+      setLoading(true);
+
+      const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current%2Cdays&key=${superSecretKey}&contentType=json`);
+      const weatherResponse = await data.json();
+
+      return weatherResponse;
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const getWeatherData = async () => {
+    try {
+      const response = await fetchWeatherData();
+
+      setWeatherData(response);
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (weatherData.currentConditions) {
+      setTemp(weatherData.currentConditions.temp);
+      setConditions(weatherData.currentConditions.conditions);
+      setIcon(weatherData.currentConditions.icon);
+    }
+  }, [weatherData])
+
   return (
-    <div className="bg-background text-text min-h-[100dvh] flex items-center justify-center">
-      <div className="flex items-center jsutify-center mr-6">
-        <Cat 
-          size={64}
-          weight="fill"
-          color="#FFF"
-        />
-      </div>
-      <div className="font-bold text-7xl text-center">
-        Cats: {cats}
+    <div className="
+      bg-background text-text min-h-[100dvh] 
+      flex flex-col items-center justify-center
+    ">
+      <input
+        type="button"
+        value="Get Data"
+        className="px-16 py-2 bg-cyan-700 font-semibold text-center cursor-pointer rounded-xl"
+        onClick={() => getWeatherData()}
+      />
+      <div>
+        {
+          !loading ?
+            <div className="
+              flex flex-col items-center justify-center mt-6
+            ">
+              <div className="flex items-center justify-center">
+                <div className="mr-2">Temp:</div>
+                <div> {temp} </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="mr-2">Condition</div>
+                <div> {conditions} </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="mr-2">Icon:</div>
+                <div> {icon} </div>
+              </div>
+            </div>
+            :
+            <div>
+              Loading...
+            </div>
+        }
       </div>
     </div>
   )
--- src/useWeatherStore.jsx
+++ src/useWeatherStore.jsx
@@ -1,7 +1,32 @@
 import { create } from 'zustand'
 
 export const useWeatherStore = create((set) => ({
-  cats: 14,
-  increasePopulation: () => set((state) => ({ cats: state.bears + 1 })),
-  removeAllCats: () => set({ cats: 0 }),
+    superSecretKey: "N98UUEZWJCFKVHRPUJNY28T7R",
+
+    loading: false,
+    setLoading: (loading) => set({ loading }),
+
+    weatherData: {},
+    setWeatherData: (value) => set({ weatherData: value }),
+
+    units: "c",
+    changeUnits: () => set((state) => ({ units: state.units === "c" ? "f" : "c" })),
+
+    location: "London",
+    changeLocation: (location) => set({ location }),
+
+    //response values
+    temp: "",
+    setTemp: (temp) => set({ temp }),
+    conditions: "",
+    setConditions: (conditions) => set({ conditions }),
+    feelsLike: "",
+    setFeelsLike: (feelsLike) => set({ feelsLike }),
+    humidity: "",
+    setHumidity: (humidity) => set({ humidity }),
+    precip: 0,
+    setPrecip: (precip) => set({ precip }),
+    icon: "",
+    setIcon: (icon) => set({ icon }),
+
 }))
